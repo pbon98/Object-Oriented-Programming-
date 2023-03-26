0c53868e327f65e3c2fb9ac553b21e38d070045b
pbon98 <pbaughan15@gmail.com>
2023-03-26 01:31:11 -0400

    object-oriented-programming

---
+++ step22.js
@@ -0,0 +1,21 @@
+let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  // Only change code below this line
+  let glideMixin = function(obj) {
+    obj.glide = function() {
+      console.log("Gliding, Woosh!")
+    }
+  }
+  
+  glideMixin(bird);
+  glideMixin(boat);
+  
+  bird.glide()
